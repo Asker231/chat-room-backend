b0a3090b0c5f4bee4c9834912c6c277b03095563
Asker <kotsevasker0@gmail.com>
2025-02-16 07:17:36 +0300

    init http

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,21 @@
 package main
 
+import (
+	"log"
+	"net/http"
+)
+
 
 func main() {
-	
+
+	//init Handler
+	router := http.NewServeMux()
+
+	//init server 
+	server := http.Server{
+		Addr: ":80",
+		Handler: router,
+	}
+
+	log.Panic(server.ListenAndServe())
 }
